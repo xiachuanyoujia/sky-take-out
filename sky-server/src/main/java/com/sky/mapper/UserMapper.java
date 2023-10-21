03d11319959d1dfd97fb688ac3597e7f00dbf694
xiachuanyoujia <1243357202@qq.com>
2023-10-21 09:39:04 +0800

    feat：微信登录

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("select * from sky_take_out.user where openid = #{openid}")
+    User getByOpenid(String openid);
+
+    /**
+     * 插入数据
+     * @param user
+     */
+    void insert(User user);
+}
